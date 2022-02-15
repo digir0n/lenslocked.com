4d727393780551375bb6532adbe1a783a5cffdf7
digir0n <ronald.de.villiers@gmail.com>
2022-02-15 21:19:39 +0200

    Installing gorilla mux

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome once again</h1>")
-	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	} else if r.URL.Path == "/contact" || r.URL.Path == "/contact/"{
 		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@mr-it.co.za\">support@mr-it.co.za</a>.")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
@@ -19,6 +19,5 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
-	http.HandleFunc("/contact", handlerFunc)
 	http.ListenAndServe(":3000", nil)
 }
