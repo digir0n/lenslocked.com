858d61b190a7ec4811db543baed6ec7b3864ab61
digir0n <ronald.de.villiers@gmail.com>
2022-02-15 21:01:45 +0200

    Installed gorilla mux

--- main.go
+++ main.go
@@ -8,17 +8,17 @@ import (
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+		fmt.Fprint(w, "<h1>Welcome once again</h1>")
 	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@mr-it.co.za\">support@mr-it.co.za</a>.")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>We could find the page you were looking for :(</h1><p>Please email us if you keep being sent to an invalid page.</p>")
+		fmt.Fprint(w, "<h1>We could not find the page you were looking for :(</h1><p> Please email us if you keep being sent to an invalid page.</p>")
 	}
 }
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
-	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.HandleFunc("/contact", handlerFunc)
 	http.ListenAndServe(":3000", nil)
 }
