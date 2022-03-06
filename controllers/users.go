50183678df987e248f46e144137dff222e13f062
digir0n <ronald.de.villiers@gmail.com>
2022-03-06 13:44:41 +0200

    Creating our 1st controller

--- controllers/users.go
+++ controllers/users.go
@@ -7,6 +7,10 @@ import (
 	"lenslocked.com/views"
 )
 
+// NewUsers is used to create a new Uses controlle.
+// This function will panic if the templates are not
+// parsed correctly, and should only be used during
+// initial setup.
 func NewUsers() *Users {
 	return &Users{
 		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
@@ -22,8 +26,9 @@ type Users struct {
 //
 // GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("is this working?")
-	u.NewView.Render(w, nil)
+	if err := u.NewView.Render(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 // This is used to process the signup form when a user tries to
